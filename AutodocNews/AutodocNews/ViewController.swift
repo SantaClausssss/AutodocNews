8e341a8ad635ed61581a6838aa865866cb5cc30b
Santa Claus <ns.object@outlook.com>
2025-05-21 15:00:05 +0300

    initialize Xcode project

---
+++ AutodocNews/AutodocNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutodocNews
+//
+//  Created by Санта Клаус on 21.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
